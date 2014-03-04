34eb57fc0df4e91ada496df4689b3f605bdef404
ghassen ticha <ghassen ticha@ghassen>
2014-03-04 21:20:17 +0100

    ye bheyemmm

--- tn.edu.esprit.infini.theWolves.tvForEx/ejbModule/tn/edu/esprit/infini/theWolves/tvForEx/domain/Rules.java
+++ tn.edu.esprit.infini.theWolves.tvForEx/ejbModule/tn/edu/esprit/infini/theWolves/tvForEx/domain/Rules.java
@@ -1,53 +1,63 @@
 package tn.edu.esprit.infini.theWolves.tvForEx.domain;
 
-
 import java.sql.Date;
 
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Rules {
 
 	private int id;
-	@Temporal(TemporalType.TIMESTAMP)
+
 	private Date date;
 	private String label;
 	private float value;
-	
+
 	private Administrator administrator;
-	
-	
+
 	public Rules() {
 		super();
 	}
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public String getLabel() {
 		return label;
 	}
+
 	public void setLabel(String label) {
 		this.label = label;
 	}
+
 	public float getValue() {
 		return value;
 	}
+
 	public void setValue(float value) {
 		this.value = value;
 	}
 
-	@OneToOne
+	@ManyToOne
 	public Administrator getAdministrator() {
 		return administrator;
 	}
@@ -56,8 +66,4 @@ public class Rules {
 		this.administrator = administrator;
 	}
 
-	
-	
-	
-	
 }
