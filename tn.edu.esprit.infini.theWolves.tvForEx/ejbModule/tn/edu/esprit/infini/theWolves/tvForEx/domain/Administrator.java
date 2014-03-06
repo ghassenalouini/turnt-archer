ba0d9079ac9e9f23f1e7c33c60c67578dfb5f8c2
ghassen ticha <ghassen ticha@ghassen>
2014-03-06 12:46:35 +0100

    commentaire

--- tn.edu.esprit.infini.theWolves.tvForEx/ejbModule/tn/edu/esprit/infini/theWolves/tvForEx/domain/Administrator.java
+++ tn.edu.esprit.infini.theWolves.tvForEx/ejbModule/tn/edu/esprit/infini/theWolves/tvForEx/domain/Administrator.java
@@ -60,35 +60,61 @@ public class Administrator {
 		this.nom = nom;
 	}
 
+	/**
+	 * c'est l getter du parametre login il ne prend rien en parametre et
+	 * retourne un String
+	 **/
 	public String getLogin() {
 		return login;
 	}
 
+	/**
+	 * c'est le setter de la classe Admin du parametre login il prend en
+	 * parametre un String
+	 **/
 	public void setLogin(String login) {
 		this.login = login;
 	}
 
+	/**
+	 * getter de la classe administrator de l'attribut password et qui retourne
+	 * un String
+	 **/
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * c'est le setter duparametre password de la classe administator qui prend
+	 * en parametre un String
+	 **/
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/** Getter de l'adresse mail et qui retourne un String **/
 	public String getEmailAddress() {
 		return emailAddress;
 	}
 
+	/** Le setter de l'adresse mail qui prend en parametre un String **/
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
 
+	/**
+	 * c'est le getter de la liste des rules qui retourne la liste des rules qui
+	 * on été placés par l'administrateur
+	 **/
 	@OneToMany(mappedBy = "administrator")
 	public List<Rules> getRules() {
 		return rules;
 	}
 
+	/**
+	 * cette methode prend en parametre une liste de rules et qui permet de
+	 * modifier =la liste des rules
+	 **/
 	public void setRules(List<Rules> rules) {
 		this.rules = rules;
 	}
--- tn.edu.esprit.infini.theWolves.tvForEx/ejbModule/tn/edu/esprit/infini/theWolves/tvForEx/domain/Bank.java
+++ tn.edu.esprit.infini.theWolves.tvForEx/ejbModule/tn/edu/esprit/infini/theWolves/tvForEx/domain/Bank.java
@@ -21,31 +21,54 @@ public class Bank extends Customer implements Serializable {
 	public Bank() {
 		super();
 	}
-	
-	public Bank(String name, String login, String password, int phone_number, String adresse_mail, int fond_propre, String swift_code) {
+
+	/**
+	 * c'est le constructeur de la classe bank qui heirte deja de la classe
+	 * Custommer qui prend en prametre le nom(String),password(String
+	 * ),phone_number(int),adresse_mail(Strin),swift_code(String)
+	 **/
+	public Bank(String name, String login, String password, int phone_number,
+			String adresse_mail, int fond_propre, String swift_code) {
 		super(name, login, password, phone_number, adresse_mail);
 		this.fond_propre = fond_propre;
 		this.swift_code = swift_code;
 	}
 
+	/**
+	 * constructeur de la classe Bank qui herite de la classe Customer et qui
+	 * prend deux parametres:fond_propre(int),swift_code(String)
+	 **/
 	public Bank(int fond_propre, String swift_code) {
 		super();
 		this.fond_propre = fond_propre;
 		this.swift_code = swift_code;
 	}
 
+	/**
+	 * c'est le getter de la classe Bank qui retourne le fond_propre d'une
+	 * banque de type int
+	 **/
 	public int getFond_propre() {
 		return fond_propre;
 	}
 
+	/**
+	 * c'est le setter du fond_propre de la classe bank qui prend en parametre
+	 * un int
+	 **/
 	public void setFond_propre(int fond_propre) {
 		this.fond_propre = fond_propre;
 	}
 
+	/** il retourne le swift_code d'une banque qui est de type String **/
 	public String getSwift_code() {
 		return swift_code;
 	}
 
+	/**
+	 * permet de modifier le swift_code d'une banque en prenant enparametre un
+	 * String
+	 **/
 	public void setSwift_code(String swift_code) {
 		this.swift_code = swift_code;
 	}
